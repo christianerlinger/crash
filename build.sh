a6226caa12726f801f54d83d0053b8d06c1110f8
Christian <aintnomail>
2021-02-27 17:26:23 +0100

    build file
    - read version number from properties file
    - build linux and windows binaries
    - zip linux and windows binaries

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 GODOT_BINARY=~/Godot_v3.2.3/Godot_v3.2.3-stable_x11.64
+PROJECT_NAME=crash
+BASEDIR=$(pwd)
+read -r GAME_VERSION<version
+echo build version $GAME_VERSION
 rm ./bin -rf
+rm ./build -rf
 mkdir -p ./bin/linux
 mkdir -p ./bin/windows
-$GODOT_BINARY --export "Linux/X11" ./bin/linux/crash.x86_64
-$GODOT_BINARY --export "Windows Desktop" ./bin/windows/crash.exe
+mkdir -p ./build
+$GODOT_BINARY --export "Linux/X11" ./bin/linux/$PROJECT_NAME.x86_64
+$GODOT_BINARY --export "Windows Desktop" ./bin/windows/$PROJECT_NAME.exe
+pushd bin/windows
+tar -zcvf $BASEDIR/build/$PROJECT_NAME-$GAME_VERSION-windows.tar.gz ./
+popd
+pushd bin/linux
+tar -zcvf $BASEDIR/build/$PROJECT_NAME-$GAME_VERSION-linux.tar.gz ./
+popd
