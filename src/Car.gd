5f19efb7b91ed5e5d9f8b1c901f9add8579b4bf0
Christian <aintnomail>
2021-03-28 21:47:53 +0200

    do not allow 180 degree turns + new car image

--- src/Car.gd
+++ src/Car.gd
@@ -1,40 +1,50 @@
 extends KinematicBody2D
 
 
-var speed = 500
+var speed = 100
 
-var drive_vec = Vector2()
+const DIRECTION_UP = Vector2(0,-1)
+const DIRECTION_DOWN = Vector2(0, 1)
+const DIRECTION_LEFT = Vector2(-1, 0)
+const DIRECTION_RIGHT = Vector2(1, 0)
+
+const DRIVE_UP = KEY_W
+const DRIVE_DOWN = KEY_S
+const DRIVE_LEFT = KEY_A
+const DRIVE_RIGHT = KEY_D
+const FIRE = KEY_SPACE
+
+
+var driving_direction = Vector2()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	drive_vec.y += 1;
+	driving_direction = Vector2.ZERO
 func _input(event):
 	
 	if event is InputEventKey and event.pressed:
-		if event.scancode == KEY_W:
-			drive_vec = Vector2()
-			drive_vec.y -= 1
-			drive_vec = drive_vec.normalized()
+		if event.scancode == DRIVE_UP && driving_direction != DIRECTION_DOWN:
+			#if driving_direction == DIRECTION_LEFT:
+				
+			driving_direction = DIRECTION_UP
+			driving_direction = driving_direction.normalized()
 			
-		if event.scancode == KEY_S:
-			drive_vec = Vector2()
-			drive_vec.y += 1
-			drive_vec = drive_vec.normalized()
+		if event.scancode == DRIVE_DOWN && driving_direction != DIRECTION_UP:
+			driving_direction = DIRECTION_DOWN
+			driving_direction = driving_direction.normalized()
 
-		if event.scancode == KEY_A:
-			drive_vec = Vector2()
-			drive_vec.x -= 1
-			drive_vec = drive_vec.normalized()
+		if event.scancode == DRIVE_LEFT && driving_direction != DIRECTION_RIGHT:
+			driving_direction = DIRECTION_LEFT
+			driving_direction = driving_direction.normalized()
 			
-		if event.scancode == KEY_D:
-			drive_vec = Vector2()
-			drive_vec.x += 1
-			drive_vec = drive_vec.normalized()
+		if event.scancode == DRIVE_RIGHT && driving_direction != DIRECTION_LEFT:
+			driving_direction = DIRECTION_RIGHT
+			driving_direction = driving_direction.normalized()
 
 
 func _physics_process(delta):
-	var collision = move_and_collide(drive_vec * speed * delta)
+	var collision = move_and_collide(driving_direction * speed * delta)
 	if collision:
 		print("crashed")
 		crashed()
